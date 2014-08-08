a665cd09562b54c2d4d026d04c96b90a34767cba
Eduardo  Díaz Cortés <ediaz@dcc8090.cl>
2014-08-07 20:51:15 -0400

    Fix bug when you don't have Amazon S3 credentials.

--- app/tools/ContentManager.scala
+++ app/tools/ContentManager.scala
@@ -4,32 +4,40 @@ import awscala._
 import awscala.s3.{Bucket, S3}
 import com.amazonaws.services.s3.model.ObjectMetadata
 import controllers.routes
-import play.api.{Logger, Play}
+import play.api.Play
 import play.api.Play.current
 
 
 object ContentManager {
 
-  implicit val s3 = S3()
 
   val cdnurl = Play.application.configuration.getString("prosa.cdn.url").orNull
 
-  val bucket: Bucket = s3.bucket("prosa-bucket").getOrElse(s3.createBucket("prosa-bucket"))
 
   def putFile(key:String, file:File, contentType:String) = {
-    if (cdnurl == null || cdnurl.isEmpty) {
-      val url = routes.ContentController.getImage(key).url
-      Logger.info("put file return "+url)
-      url
-    }
-    else {
-      val source = scala.io.Source.fromFile(file)(scala.io.Codec.ISO8859)
-      val byteArray = source.map(_.toByte).toArray
-      val metadata: ObjectMetadata = new ObjectMetadata()
-      metadata.setContentType(contentType)
-      metadata.setContentLength(byteArray.length)
-      bucket.putObjectAsPublicRead(key, byteArray, metadata)
-      cdnurl + key
-    }
+    if (cdnurl == null || cdnurl.isEmpty)
+      routes.ContentController.getImage(key).url
+    else
+      // patch
+      AmazonS3.putFile(cdnurl, key, file, contentType)
   }
+
+}
+
+
+private object AmazonS3 {
+
+  lazy implicit val s3 = S3()
+
+  def putFile(cdnurl: String, key: String, file: File, contentType: String) = {
+    val bucket: Bucket = s3.bucket("prosa-bucket").getOrElse(s3.createBucket("prosa-bucket"))
+    val source = scala.io.Source.fromFile(file)(scala.io.Codec.ISO8859)
+    val byteArray = source.map(_.toByte).toArray
+    val metadata: ObjectMetadata = new ObjectMetadata()
+    metadata.setContentType(contentType)
+    metadata.setContentLength(byteArray.length)
+    bucket.putObjectAsPublicRead(key, byteArray, metadata)
+    cdnurl + key
+  }
+
 }
