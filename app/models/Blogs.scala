f0aab3bb99fe21b30c7aa091f25c0e03237b39d2
Eduardo  Díaz Cortés <ediaz@dcc8090.cl>
2016-01-10 00:49:32 -0300

    Implementa PROSA-16. Upgrade de Play Framework. And various goodies.

--- app/models/Blogs.scala
+++ app/models/Blogs.scala
@@ -31,7 +31,8 @@ case class Blog(
                  disqus:Option[String],
                  googleAnalytics:Option[String],
                  status:BlogStatus.Value, //
-                 owner:String
+                 owner:String,
+                 twitter:Option[String]
                ) extends Identifiable
 
 
@@ -49,8 +50,9 @@ class Blogs(tag:Tag) extends Table[Blog](tag, "blog") with HasId {
   def googleAnalytics = column[Option[String]]("google_analytics")
   def status = column[BlogStatus.Value]("status")
   def owner = column[String]("owner", O.Length(45, varying = true))
+  def twitter = column[Option[String]]("twitter_handle")
 
-  def * = (id,name,alias,description,image, logo, url, useAvatarAsLogo, disqus, googleAnalytics, status, owner) <> (Blog.tupled, Blog.unapply)
+  def * = (id,name,alias,description,image, logo, url, useAvatarAsLogo, disqus, googleAnalytics, status, owner, twitter) <> (Blog.tupled, Blog.unapply)
 }
 
 
@@ -73,8 +75,8 @@ object Blogs extends DbService[Blog]{
   def findByAlias(alias:String) : Future[Option[Blog]] =
     dbConfig.db.run(blogs.filter(_.alias === alias).result.headOption)
 
-  def create(owner:Author, name:String,alias:String,description:String,image:Option[String],logo:Option[String],url:Option[String], disqus:Option[String], gogleAnalytics:Option[String], useAvatarAsLogo:Option[Boolean]) : Future[Int] =
-    insert(Blog(IdGenerator.nextId(classOf[Blog]), name, alias, description, image, logo, url, useAvatarAsLogo, disqus, gogleAnalytics, BlogStatus.CREATED, owner.id))
+  def create(owner:Author, name:String,alias:String,description:String,image:Option[String],logo:Option[String],url:Option[String], disqus:Option[String], gogleAnalytics:Option[String], useAvatarAsLogo:Option[Boolean], twitter:Option[String]) : Future[Int] =
+    insert(Blog(IdGenerator.nextId(classOf[Blog]), name, alias, description, image, logo, url, useAvatarAsLogo, disqus, gogleAnalytics, BlogStatus.CREATED, owner.id, twitter))
 
   def update(blog:Blog, name:String,alias:String,description:String,image:Option[String],logo:Option[String],url:Option[String], disqus:Option[String], gogleAnalytics:Option[String], useAvatarAsLogo:Option[Boolean], status:BlogStatus.Value) : Future[Int] =
     update (blog.copy(name=name, alias=alias,  useAvatarAsLogo=useAvatarAsLogo, description=description, image=image, logo=logo, url=url, disqus=disqus, googleAnalytics=gogleAnalytics, status=status))
