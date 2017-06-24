ef63dee1ac7bbae5f86badeb976129947a0c93fc
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2017-06-24 15:01:57 -0400

    visualiza los anuncios en cada post

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -2,8 +2,10 @@
 
 # --- !Ups
 
-ALTER TABLE blog ADD twitter_handle text;
+ALTER TABLE blog ADD show_ads boolean;
+ALTER TABLE blog ADD ads_code text;
 
 # --- !Downs
 
-ALTER TABLE blog DROP twitter_handle;
+ALTER TABLE blog DROP show_ads;
+ALTER TABLE blog DROP ads_code;
